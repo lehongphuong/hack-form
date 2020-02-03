5b7942542d73a6cda82b5af511bbca153d433d8b
Le Hong Phuong <helloitpdu@gmail.com>
2020-02-03 11:11:31 +0700

    Add first hack phong ve tau saky

--- common.py
+++ common.py
@@ -20,9 +20,9 @@ mock_email = ['tandat1957@gmail.com','sunsolution283@gmail.com','trandinhtrong.2
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--no-sandbox")
 
 
 def phongvetaulyson(number): 
