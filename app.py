76906da139e1e3aa9681dc00ae48fe9808e33b0e
PinkWays <helloitpdu@gmail.com>
2020-07-26 22:25:42 +0700

    update remove database

--- app.py
+++ app.py
@@ -51,4 +51,5 @@ def index():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(debug=True, threaded=True, port=5000)
+    app.run(host="0.0.0.0", debug=True)
+    # app.run(debug=True, threaded=True, port=5000)
